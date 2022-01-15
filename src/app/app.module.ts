bd2bfc436fcc03a891028cdfd8cfbddd3ad6c005
Camila Fonseca <camilarqf@gmail.com>
2022-01-14 22:03:28 -0300

    feat: adiciona model usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { UsuarioListComponent } from './components/usuario/usuario-list/usuario-list.component';
-
-
 
 
 @NgModule({
@@ -27,7 +28,11 @@ import { UsuarioListComponent } from './components/usuario/usuario-list/usuario-
     MatListModule,
     MatIconModule,
     MatMenuModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/models/usuario.ts
@@ -0,0 +1,12 @@
+export interface Usuario {
+  id?: any;
+  nome: string;
+  cpf: string;
+  matricula: string;
+  telefone: string;
+  email: string;
+  endereco: string;
+  perfis: string[];
+  senha: string;
+  dataCriacao: any;
+}
