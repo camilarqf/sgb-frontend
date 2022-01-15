4cb93819dc18e20c3feb3177d1c0be07420a8985
Camila Fonseca <camilarqf@gmail.com>
2022-01-14 21:31:01 -0300

    feat: finaliza componente sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
+
+
+
 
 @NgModule({
   declarations: [AppComponent, SidenavComponent],
@@ -14,6 +22,11 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -11,5 +11,5 @@ export class SidenavComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  
 }
