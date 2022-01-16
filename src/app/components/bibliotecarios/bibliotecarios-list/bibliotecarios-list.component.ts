b5ce9a351142bbc136b91bfc92a03d878a2fc62a
Camila Fonseca <camilarqf@gmail.com>
2022-01-16 18:46:09 -0300

    feat: finaliza tela de listagem de bibliotecarios

--- src/app/components/bibliotecarios/bibliotecarios-list/bibliotecarios-list.component.ts
+++ src/app/components/bibliotecarios/bibliotecarios-list/bibliotecarios-list.component.ts
@@ -1,7 +1,9 @@
 import { BibliotecarioService } from './../../../service/bibliotecario.service';
 import { Bibliotecario } from './../../../models/bibliotecario';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-bibliotecarios-list',
@@ -10,6 +12,11 @@ import { MatTableDataSource } from '@angular/material/table';
 })
 export class BibliotecariosListComponent implements OnInit {
   DATA: Bibliotecario[] = [];
+  displayedColumns: string[] = ['id', 'nome', 'cpf', 'matricula', 'acoes'];
+  loading: boolean = true;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(private service: BibliotecarioService) {}
 
@@ -19,10 +26,25 @@ export class BibliotecariosListComponent implements OnInit {
     console.log(this.dataSource);
   }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
   findAll(): void {
     this.service.findAll().subscribe((response) => {
       this.DATA = response;
       this.dataSource = new MatTableDataSource<Bibliotecario>(this.DATA);
+      this.loading = false;
     });
   }
 }
